c425dd5fcbb8fc9e9ea699445b8c1a2f5dd232db
Andre Santos Barros da Silva <andrezio@live.com>
2018-05-07 16:02:12 -0300

    update move ship with mouse

--- scripts/ship.gd
+++ scripts/ship.gd
@@ -1,20 +1,15 @@
 extends Area2D
 
-var pos = Vector2()
-
 func _ready():
 	set_process(true)
 
 func _process(delta):
-	pos.x = get_local_mouse_position().x
+	var pos = position.x
+	var mousex = get_global_mouse_position().x
+	var motion = (mousex-pos) * 0.2
+	translate(Vector2(motion,0))
+	#pos = clamp(pos,16.0,100)
+	#move_local_x(pos)
+	#print(pos)
 	
-func _physics_process(delta):
 	
-	#move_local_x(pos.x)
-	
-	#var view_size = get_viewport_rect().size
-	#var view_size = get_viewport().size.x
-
-	#pos.x = clamp(pos.x,16.0,view_size-16.0)
-	#print(view_size)
-	translate(pos)
